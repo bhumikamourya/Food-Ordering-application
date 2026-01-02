d14902dc7e327b794e5e90834a565bfe5b37819b
bhumikamourya <bhumikamourya275@gmail.com>
2026-01-02 22:09:56 +0530

    improve styling

--- app.js
+++ app.js
@@ -31,44 +31,44 @@ app.get("/demo", (req, res) => {
    res.render("listings/demo.ejs");
 })
 //front page of our app
-app.get("/", (req, res) => {
-   res.render("listings/home.ejs");
+app.get("/",(req, res) => {
+   res.render("pages/home.ejs");
 
 })
 //signup page
 app.get("/signup", (req, res) => {
-   res.render("listings/signup.ejs");
+   res.render("pages/signup.ejs");
 })
 //login page
 app.get("/login", (req, res) => {
-   res.render("listings/login.ejs");
+   res.render("pages/login.ejs");
 })
 //home page of our app
 app.get("/home-menu",wrapAsync (async (req, res ,next) => {
-   const alllisting = await Listing.find({});
-   res.render("listings/home-menu.ejs", { alllisting });
+   const allItems = await Listing.find({});
+   res.render("pages/home-menu.ejs", { allItems });
 }));
 //about our app
 app.get("/home-menu/about", (req, res) => {
-   res.render("listings/about.ejs");
+   res.render("pages/about.ejs");
 })
 //new route
 app.get("/home-menu/new", (req, res) => {
-   res.render("listings/new.ejs");
+   res.render("pages/new.ejs");
 })
 
 //show route - food details
 app.get("/home-menu/:id",wrapAsync (async (req, res,next) => {
    let { id } = req.params;
-   const listing = await Listing.findById(id);
-   res.render("listings/food-details.ejs", { listing });
+   const Items = await Listing.findById(id);
+   res.render("pages/food-details.ejs", { Items });
 }));
 //create route
-// app.post("/home-menu", async (req, res) => {
-//       const newListing = new Listing(req.body.listing);
-//       await newListing.save();
-//       res.redirect("listings/home-menu.ejs");
-// });
+app.post("/home-menu", async (req, res) => {
+      const newItem = new Listing(req.body.listing);
+      await newItem.save();
+      res.redirect("pages/home-menu.ejs");
+});
 
 //edit route
 app.get("/home-menu/:id/edit",wrapAsync( async (req, res) => {
@@ -76,7 +76,7 @@ app.get("/home-menu/:id/edit",wrapAsync( async (req, res) => {
    const listing = await Listing.findById(id);
    let originalImageUrl = listing.Image;
    originalImageUrl=originalImageUrl.replace("/upload" ,"/upload/w_250");
-   res.render("listings/edit.ejs" ,{listing , originalImageUrl});
+   res.render("pages/edit.ejs" ,{listing , originalImageUrl});
 }))
 //update route
 // app.put("/home-menu/:id" ,async(req ,res)=>{
